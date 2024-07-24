f09f57ba353e52ce5d5ed6b85a9a98d9512fbf67
Daniel-Alvarenga <daniel07alvarenga2023@gmail.com>
2024-07-24 20:33:53 -0300

    First commit

---
+++ site/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img src="https://github.com/Daniel-Alvarenga/Follcker/blob/main/src/assets/source/image/logo.png" alt="Follcker" />
+  <Page />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Page from './components/Page.vue';
+
+@Options({
+  components: {
+    Page,
+  },
+})
+export default class App extends Vue { }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
